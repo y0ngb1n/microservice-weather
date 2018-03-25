6e9372ed9c0ad38867b921c5042b51de8dafabca
ybin <ybin1212@qq.com>
2018-03-25 10:22:24 +0800

    init project.

---
+++ src/main/java/com/study/demo/microservice/weather/Application.java
@@ -0,0 +1,12 @@
+package com.study.demo.microservice.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceWeatherApplication.class, args);
+	}
+}
