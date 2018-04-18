7e90383c79cb0bbf2603804c428525fea1e94c60
ybin <ybin1212@qq.com>
2018-04-18 18:10:55 +0800

    使用 Redis 提升应用的并发访问能力.

--- src/main/java/com/study/demo/microservice/weather/service/impl/WeatherDataServiceImpl.java
+++ src/main/java/com/study/demo/microservice/weather/service/impl/WeatherDataServiceImpl.java
@@ -5,12 +5,15 @@ import com.study.demo.microservice.weather.service.WeatherDataService;
 import com.study.demo.microservice.weather.vo.WeatherResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yangbin
@@ -20,9 +23,12 @@ import java.io.IOException;
 public class WeatherDataServiceImpl implements WeatherDataService {
 
     private final static String WEATHER_PROVIDER_URL = "http://wthrcdn.etouch.cn/weather_mini?";
+    private static final Long CACHE_TIMEOUT_SECONDS = 1800L;
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
@@ -43,15 +49,30 @@ public class WeatherDataServiceImpl implements WeatherDataService {
      * @return WeatherResponse
      */
     private WeatherResponse getWeatherData(String url) {
-        log.debug("【获取天气数据】正在请求：{}", url);
-        // 获取天气数据
-        ResponseEntity<String> entity = restTemplate.getForEntity(url, String.class);
+        String cacheKey = String.format("weather:data:%s",
+                url.replace(WEATHER_PROVIDER_URL, "").replace('=', ':'));
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        String json = null;
         WeatherResponse weatherResponse = null;
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = null;
-        if (entity.getStatusCode() == HttpStatus.OK) {
-            json = entity.getBody();
+
+        // 1. 从缓存中获取
+        if (stringRedisTemplate.hasKey(cacheKey)) {
+            log.debug("【获取天气数据】从缓存中获取：{}", url);
+            json = ops.get(cacheKey);
         }
+        // 2. 缓存中没有数据, 则从调用服务接口进行获取
+        else {
+            log.debug("【获取天气数据】正在请求：{}", url);
+            // 获取天气数据
+            ResponseEntity<String> entity = restTemplate.getForEntity(url, String.class);
+            if (entity.getStatusCode() == HttpStatus.OK) {
+                json = entity.getBody();
+                // 将数据写入缓存
+                ops.set(cacheKey, json, CACHE_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            }
+        }
+
         // json to entity.
         try {
             weatherResponse = objectMapper.readValue(json, WeatherResponse.class);
