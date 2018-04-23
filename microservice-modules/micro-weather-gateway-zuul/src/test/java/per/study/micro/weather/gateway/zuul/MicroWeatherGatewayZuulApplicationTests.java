5048282658044bf3e3b057c089fbef44fca49699
ybin <ybin1212@qq.com>
2018-04-23 11:12:27 +0800

    实现 API 网关（Zuul）

---
+++ microservice-modules/micro-weather-gateway-zuul/src/test/java/per/study/micro/weather/gateway/zuul/MicroWeatherGatewayZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package per.study.micro.weather.gateway.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroWeatherGatewayZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
